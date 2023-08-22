33485d7048ff58407c26fd61d805a8a0d1ce3af0
Jean Bardellini <jbardellini@LABA.PSWAS.LOCAL>
2023-08-22 11:06:35 -0700

    finish lab with if statement trial

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const h1Element = document.querySelector("h1");
 h1Element.textContent = "Dom Mini-Lab";
 const hideElement = document.querySelector("#hide");
 const unhideElement = document.querySelector("#unhide");
-
+const btn = document.querySelectorAll("button");
 hideElement.addEventListener("click", () => {
   h1Element.style.display = "none";
 });
 unhideElement.addEventListener("click", () => {
   h1Element.style.display = "block"; // or use removeProperty("display") instead of style property
 });
+
+// let hiddenButton = true;
+
+// const btnFunction = () => {
+//   if (!hiddenButton) {
+//     hiddenButton = true;
+//     h1Element.style.display = "none";
+//   } else {
+//     hiddenButton = false;
+//     h1Element.style.display = "block";
+//   }
+// }; // stuck, will research later
+// btn.addEventListener("click", btnFunction);
